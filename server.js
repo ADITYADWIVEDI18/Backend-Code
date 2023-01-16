5735b186fac181113846e6f59f276286ad927560
Aditya Dwivedi <104710359+ADITYADWIVEDI18@users.noreply.github.com>
2023-01-16 22:58:47 +0530

    Add files via upload
    This is an Calculator Code for addition of two numbers using express js.

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const bodyParser=require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get("/" , function(request,response){
 
-    response.send("Hello World")
+        response.sendFile(__dirname + "/index.html")
 })
+
+app.get("/contact",function(req,res){
+    res.send("number likho")})
+
+app.post("/" , function(req,res) {
+
+    var num1=   Number(req.body.n1);
+    var num2 =  Number(req.body.n2);
+    var result= num1+num2;
+    res.send("Calculation is "+result);
+
+    
+        })
 
 app.listen(3000 , function(){
      console.log("server started on port 3000");
