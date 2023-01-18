8e6f5434dcbc1f18769bed7fba7ab00fc39ad446
Aditya Dwivedi <104710359+ADITYADWIVEDI18@users.noreply.github.com>
2023-01-19 02:16:10 +0530

    Add files via upload

---
+++ BMI express/server.js
@@ -0,0 +1,34 @@
+//jshint esversion: 6 
+
+const express=require('express');
+
+const bodyParser=require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.listen(3000 , function(){
+    console.log("server started on port 3000");
+})
+
+app.get("/" , function(request,response){
+
+    response.sendFile(__dirname + "/index.html")
+})
+
+app.post("/" , function(req,res) {
+
+
+    var wolo =Number(req.body.weights);
+    var feets=Number(req.body.feet);
+    var inc=Number(req.body.inch);
+    var result = Number(((feets*12) + inc)*0.0254);
+    
+    var finalres=   Number(result*result);
+    var result = Number(wolo / finalres);
+    
+    res.send("Calculation is "+ result);
+
+})
