dc2f57a6bfd56103c345c64b0792a28511cb9cad
Aditya Dwivedi <104710359+ADITYADWIVEDI18@users.noreply.github.com>
2023-01-29 15:31:12 +0530

    Add files via upload

---
+++ WeatherReport(mini)/app.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const app = express()
+const bodyParser=require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));  //this is just necessary code for us to be able to start parsing through the body of the post request
+const https =   require("https")
+
+app.get("/",function(req,res){
+
+    res.sendFile(__dirname + "/index.html");
+    
+
+})
+
+app.post("/", function(req,res){
+
+    const query= req.body.cityName;
+    const apiKey="123e19067e20e14a7a86e436ee24cdaa";
+    const unit = "metric";
+    const url="https://api.openweathermap.org/data/2.5/weather?q=" + query +"&appid=" + apiKey +"&units=" + unit;
+
+    https.get(url,function(response){
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+
+            const weatherData = JSON.parse(data);
+            
+            const tempe =weatherData.main.temp;
+            
+            res.write("<h3> By Aditya Dwivedi </h3>");
+            res.write("<h1>The weather temperature is "+ tempe +"</h1>");
+            res.send();
+
+
+        })
+    })
+})
+
+app.listen(3000,function(){
+    console.log("Server is running on the port 3000.");
+})
+
+
+
+
