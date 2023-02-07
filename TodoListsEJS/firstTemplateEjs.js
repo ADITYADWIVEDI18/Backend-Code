0038f48c0052d499a283688e45716f44f3e96de4
Aditya Dwivedi <104710359+ADITYADWIVEDI18@users.noreply.github.com>
2023-02-08 00:19:41 +0530

    Create firstTemplateEjs.js

---
+++ TodoListsEJS/firstTemplateEjs.js
@@ -0,0 +1,51 @@
+/*jshint esversion: 6 */
+
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+app.set('view engine', 'ejs');
+
+
+app.listen(3000, function () {
+    console.log("Server Started on port 3000");
+})
+
+app.get("/", function (req, res) {
+
+    var today = new Date();
+    var day = "";
+    var currentDay = today.getDay();
+
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+
+        case 1:
+            day = "Monday";
+            break;
+
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break
+
+    }
+
+    res.render('lists', { kindOfDay: day });
+})
+
