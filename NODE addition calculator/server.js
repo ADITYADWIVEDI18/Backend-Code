3398164224c346e90ab9b909b032c6535075a03b
Aditya Dwivedi <104710359+ADITYADWIVEDI18@users.noreply.github.com>
2023-06-18 07:52:12 +0530

    Add files via upload

---
+++ NODE addition calculator/server.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+const express = require("express"); 
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+        res.send(__dirname + "/index.html");
+})
+
+app.post("/" , function(req,res){
+        var num1 = Number(req.body.n1);
+        var num2 = Number(req.body.n2);
+        var result = num1 + num2;
+
+        res.send("The sum of the numbers are" + result);
+});
+
+
+app.listen(3000 , function(req,res){
+        console.log("Server starte on port 3000")
+})
+
